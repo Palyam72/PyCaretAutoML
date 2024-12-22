6e6a96a5cb09a771a5473c9ef9bf2f5f546527b8
ROHITH <pythonchannel250104@gmail.com>
2024-12-22 09:22:19 +0530

    Update CLASSIFICATION.py

--- CLASSIFICATION.py
+++ CLASSIFICATION.py
@@ -1,87 +1,99 @@
+## classification.py
 import streamlit as st
 import pandas as pd
 from pycaret.classification import *
 
-classificationList = ["setup_cls","create_cls_df", "create_cls","predict_cls","predict_cls_df", "ensemble_cls","ensemble_cls_df", "tune_cls","tune_cls_df", "save_create_cls", "save_enseble_cls", "save_tune_cls"]
-for i in classificationList:
-    if i not in st.session_state:
-        st.session_state[i] = None
+# Initialize session state for classification-related variables
+classification_keys = [
+    "setup_cls", "compare_cls", "create_cls", "create_cls_df", "predict_cls", 
+    "predict_cls_df", "ensemble_cls", "ensemble_cls_df", "tune_cls", "tune_cls_df"
+]
+for key in classification_keys:
+    if key not in st.session_state:
+        st.session_state[key] = None
+
 class Classification:
     def __init__(self, dataset):
         self.dataset = dataset
-        self.list = ['lr', 'knn', 'nb', 'dt', 'svm', 'rbfsvm', 'gpc', 'mlp', 'ridge', 'rf', 'qda', 'ada', 'gbc', 'lda', 'et', 'xgboost', 'lightgbm', 'catboost']
+        self.models_list = [
+            'lr', 'knn', 'nb', 'dt', 'svm', 'rbfsvm', 'gpc', 'mlp', 'ridge', 
+            'rf', 'qda', 'ada', 'gbc', 'lda', 'et', 'xgboost', 'lightgbm', 'catboost'
+        ]
 
-    def selectTarget(self):
+    def select_target(self):
         if st.session_state.setup_cls is None:
-            st.subheader("First setup your environment")
+            st.subheader("Set up your environment")
             st.divider()
-            target = st.pills("Select the target column", self.dataset.columns)
+            target = st.selectbox("Select the target column", self.dataset.columns)
             if target:
                 st.session_state.setup_cls = setup(data=self.dataset, target=target)
-                st.session_state.setup_cls = pull()
-                st.dataframe(st.session_state.setup_cls)
+                st.session_state.setup_summary = pull()
+                st.dataframe(st.session_state.setup_summary)
         else:
-            st.subheader("Target column is already selected")
-            st.dataframe(st.session_state.setup_cls)
+            st.subheader("Environment already set up")
+            st.dataframe(st.session_state.setup_summary)
 
-    def compareModels(self):
+    def compare_models(self):
         if st.session_state.compare_cls is None and st.session_state.setup_cls is not None:
             st.session_state.compare_cls = compare_models()
-            st.session_state.compare_cls = pull()
-            st.dataframe(st.session_state.compare_cls)
+            st.session_state.compare_summary = pull()
+            st.dataframe(st.session_state.compare_summary)
+        elif st.session_state.compare_cls is not None:
+            st.markdown("Models comparison already performed")
+            st.dataframe(st.session_state.compare_summary)
         else:
-            st.markdown("Comparison of models is already done")
-            st.dataframe(st.session_state.compare_cls)
+            st.warning("Set up your environment first")
 
-        if st.session_state.setup_cls is None:
-            st.markdown("First select Target-->Compare models --> Create models then next steps")
-
-    def createModel(self):
-        self.selectTarget()
-        self.compareModels()
+    def create_model(self):
+        self.select_target()
+        self.compare_models()
         if st.session_state.create_cls is None and st.session_state.setup_cls is not None:
-            choosedModel = st.pills("Select the desired model that you want to assess", self.list)
-            if choosedModel:
-                st.session_state.create_cls = create_model(choosedModel)
+            selected_model = st.selectbox("Choose a model to create", self.models_list)
+            if selected_model:
+                st.session_state.create_cls = create_model(selected_model)
                 st.session_state.create_cls_df = pull()
                 st.dataframe(st.session_state.create_cls_df)
+
                 st.session_state.predict_cls = predict_model(st.session_state.create_cls)
                 st.session_state.predict_cls_df = pull()
                 st.dataframe(st.session_state.predict_cls_df)
-        else:
-            st.info("Model is already created")
+        elif st.session_state.create_cls is not None:
+            st.info("Model already created")
             st.dataframe(st.session_state.create_cls_df)
             st.dataframe(st.session_state.predict_cls_df)
 
-    def ensembleModel(self):
+    def ensemble_model(self):
         if st.session_state.ensemble_cls is None and st.session_state.create_cls is not None:
             st.session_state.ensemble_cls = ensemble_model(st.session_state.create_cls)
             st.session_state.ensemble_cls_df = pull()
             st.dataframe(st.session_state.ensemble_cls_df)
+        elif st.session_state.ensemble_cls is not None:
+            st.info("Ensemble model already created")
+            st.dataframe(st.session_state.ensemble_cls_df)
         else:
-            st.info("Ensemble model is already created or not applicable")
-            if st.session_state.ensemble_cls is not None:
-                st.dataframe(st.session_state.ensemble_cls_df)
+            st.warning("Create a base model first")
 
-    def tuneModel(self):
+    def tune_model(self):
         if st.session_state.tune_cls is None and st.session_state.create_cls is not None:
             st.session_state.tune_cls = tune_model(st.session_state.create_cls)
             st.session_state.tune_cls_df = pull()
             st.dataframe(st.session_state.tune_cls_df)
+        elif st.session_state.tune_cls is not None:
+            st.info("Model already tuned")
+            st.dataframe(st.session_state.tune_cls_df)
         else:
-            st.info("Model is already tuned or not applicable")
-            if st.session_state.tune_cls is not None:
-                st.dataframe(st.session_state.tune_cls_df)
-    def runApp(self):
+            st.warning("Create a base model first")
+
+    def run_app(self):
         col1, col2 = st.columns([1, 2])
 
         with col1:
-            options = st.pills("Select the stage", ["Create model", "Ensemble Model", "Tune Model"])
+            option = st.radio("Choose an action", ["Create Model", "Ensemble Model", "Tune Model"])
+
         with col2:
-            classification = Classification(dataset=pd.DataFrame())  # Replace with your actual dataset
-            if options == "Create model":
-                classification.createModel()
-            if options == "Ensemble Model":
-                classification.ensembleModel()
-            if options == "Tune Model":
-                classification.tuneModel()
+            if option == "Create Model":
+                self.create_model()
+            elif option == "Ensemble Model":
+                self.ensemble_model()
+            elif option == "Tune Model":
+                self.tune_model()
